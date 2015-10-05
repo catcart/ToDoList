f91db6b27e62cf4639d7e5ceaafc2eebf9d18a03
Amelia <frevert.amelia@gmail.com>
2015-10-05 09:42:41 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
     response.sendFile(__dirname + '/public/views/index.html')
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT, function(){
     var port = server.address().port;
     console.log('Listening on port: ', port);
 });
